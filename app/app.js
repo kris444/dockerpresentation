5ec6cd760c2b4aa649bcb1e66d4d67d8c4e6b465
pvmtest <swethusetty@gmail.com>
2016-08-07 23:22:26 +0530

    first commit

---
+++ app/app.js
@@ -0,0 +1,8 @@
+(function () {
+   Flowtime.start();
+
+   Flowtime.showProgress(true);
+   Flowtime.fragmentsOnBack(true);
+   Flowtime.useOverviewVariant(true);
+  Flowtime.rememberSectionsStatus(true);
+})();
